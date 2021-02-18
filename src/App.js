c38fd62a58b857fdb74c986140ec09cbdde513a6
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-02-17 22:56:55 -0400

    Creacion de la vista para detalle de usuario

--- src/App.js
+++ src/App.js
@@ -8,8 +8,16 @@ import UserState from './context/user/userState'
 const App = ()=> {
   return (
     <UserState>
-    <UserList/>
-    <Profile/>
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-md-7">
+            <UserList/>
+          </div>
+          <div className="col-md-5">
+            <Profile/>
+          </div>
+        </div>
+      </div>
     </UserState>
   );
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,10 +1,19 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import UserContext from '../context/user/userContext'
 function Profile() {
+    const {selectedUser} = useContext(UserContext); 
     return (
-        <div>
-            Profile
-        </div>
+        <>
+            {selectedUser? (
+                <div className="card card-body text-center">
+                    <img src={selectedUser.avatar} className="card-img-top rounded-circle m-auto img-fluid" style={{width:180}} alt=""/>
+                    <h1>{`${selectedUser.first_name} ${selectedUser.last_name}`}</h1>
+                    <h3>{selectedUser.email}</h3>
+                </div>
+            ):(
+                <h1>No user selected</h1>
+            )}
+        </>
     )
 }
 
